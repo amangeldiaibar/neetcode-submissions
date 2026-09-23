0267279c79e4a9895ff658b585ef1186d1ce3de3
Aibar <amangeldiaibars67@gmail.com>
2026-09-23 12:36:14 +0500

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    // Time: O(n) Space: O(n)
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2*n];
+        for(int i = 0;i < 2*n;i++){
+            if(i >= n){
+                result[i] = nums[i-n];
+            }
+            else{
+                result[i] = nums[i];
+            }
+            
+        }
+        return result;
+    }
+}
